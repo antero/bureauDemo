a454158de6d05a67a6d5202ecf907a76bdbb8ba9
Vitor Antero <vitor.melo@gmail.com>
2020-04-17 20:56:01 -0300

    Change directory structure

---
+++ documentSearcher/src/main/java/com/bw/documentSearcher/DocumentSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.bw.documentSearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentSearcherApplication.class, args);
+	}
+
+}
