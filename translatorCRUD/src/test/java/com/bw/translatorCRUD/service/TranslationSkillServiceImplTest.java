240609823f6b8227157203642cc38b01a14a6e63
Vitor Antero <vitor.melo@gmail.com>
2020-04-19 10:37:10 -0300

    Add in memory database for testing TranslatorCRUD

--- translatorCRUD/src/test/java/com/bw/translatorCRUD/service/TranslationSkillServiceImplTest.java
+++ translatorCRUD/src/test/java/com/bw/translatorCRUD/service/TranslationSkillServiceImplTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,7 +21,15 @@ class TranslationSkillServiceImplTest {
 
     @Test
     void findByExistentLanguagePair() {
-        translationSkillService.create("pt_Br", "en_uS");
+        translationSkillService.create("pt_br", "en_us");
         assertTrue(translationSkillService.findByLanguagePair("pt_br", "en_us").isPresent());
+    }
+
+    @Test
+    void getOrCreateLanguagePairDoesNotCreateRepeated() {
+        TranslationSkill ts1 =  translationSkillService.create("no_lang1", "no_lang2");
+        TranslationSkill ts2 =  translationSkillService.getOrCreateLanguagePair("no_lang1", "no_lang2");
+
+        assertEquals(ts1.getId(), ts2.getId());
     }
 }
